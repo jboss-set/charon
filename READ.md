9d075925747291c849f53c316df0f983ade02047
baranowb <baranowb@gmail.com>
2016-11-16 11:00:38 +0100

    Initial comit

---
+++ READ.md
@@ -0,0 +1,48 @@
+Charon
+===========
+Simple report tool which generate report on state of maintenance branch in repository.
+
+# Contributing
+------------
+Contributions welcome, but make sure your code passes checkstyle and respects the [formatting style](https://github.com/wildfly/wildfly-core/tree/master/ide-configs/eclipse/formatter.xml) before submitting a PR.  Furthermore, all new files must contain the JBOSS copyright notice, templates for different IDEs can be found [here](https://github.com/wildfly/wildfly-core/tree/master/ide-configs/eclipse).
+
+## Commit Guidelines
+Where possible, please try to link a commit to the GitHub issue that it aims to solve.  Commit messages should be in the format "Issue #\<Insert issue number here\>: \<Insert relevant message\>". Note, ensure that there is a space before "#<Issue number>" so that GitHub can automatically transform the string into a link to the relevant issue. 
+
+#Configuration
+------------
+Currently Charon is just a script which depends on:
+* GIT
+* SVN
+* BASH
+* CLOC 
+	* https://github.com/AlDanial/cloc
+	
+All above dependencies must be met in order  to run.
+
+#Input
+------------
+Accepted input must be in form of regular CSV values. Default delimiter character is ';'.
+Record values are as follows:
+* ID of entry
+* Contact URI
+* Repository type: GIT,SVN, ARCHIVE
+* Repository URL
+* Maintenance branch if any
+* Tag present in EAP - if any
+* Maven GroupID
+* Difference in commits between maintenance branch and tag
+* CLOC report
+* Comment
+
+#Execution
+Simply call script with path to input file, as follows:
+```
+report.sh ./config/inputEAP7.x.txt
+```
+
+#Outcome
+Report should be present in **report** directory.
+
+
+
---
+++ report.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+# sh does not seem to have array/split operator, other than awk?
+# That is why bash is in action
+
+
+#directory to which we will checkout git repos and do magic.
+REPO_DIR="tmp-repo"
+INIT_DIR="`pwd`"
+OUTPUT_DIR="report"
+OUTPUT_CODE_DIR="_code_"
+OUTPUT_FILE_HTML="report.html"
+OUTPUT_HANDLE="$OUTPUT_DIR/$OUTPUT_FILE_HTML"
+
+
+REPORT_INIT="config/report_prefix.txt"
+REPORT_END="config/report_footer.txt"
+START=$(date +%s)
+
+
+init_script(){
+    clean
+    mkdir "$OUTPUT_DIR"
+    mkdir "$OUTPUT_DIR/$OUTPUT_CODE_DIR"
+    cat $REPORT_INIT >> "$OUTPUT_HANDLE"
+}
+
+clean(){
+    cd $INIT_DIR
+    rm -rf $OUTPUT_DIR
+    rm -rf $REPO_DIR
+}
+
+checkoutSVN(){
+    rm -rf $REPO_DIR
+    if [ "$1" = "" ]; then
+        return 0
+    elif [ "$2" = "" ]; then
+        return 0
+    fi
+    
+    svn checkout $1/$2 $REPO_DIR
+}
+
+checkoutGIT(){
+    rm -rf $REPO_DIR
+    if [ "$1" = "" ]; then
+        return 0
+    fi
+    git clone $1 $REPO_DIR
+    return 0
+}
+
+getLinesOfCode(){
+    FILE="$OUTPUT_CODE_DIR/$1".txt
+    cloc $REPO_DIR --quiet --report-file="$OUTPUT_DIR/$FILE"_
+    #CLOC adds some trash in first line, remove it
+    tail -n +2 "$OUTPUT_DIR/$FILE"_ >> "$OUTPUT_DIR/$FILE"
+    rm "$OUTPUT_DIR/$FILE"_
+    echo $FILE
+}
+
+getGITCommitsFromBranch(){
+
+    if [ "$1" = "" ]; then
+        return 0
+    elif [ "$2" = "" ]; then
+        return 0
+    fi
+
+    cd $REPO_DIR
+    git checkout $1
+
+    #NOTE: This will include "Merge commit " messages, something to tinker with
+    git --no-pager log --pretty=oneline   tags/$2...origin/$1
+    COMMIT_NUMBER=`git log --pretty=oneline   tags/$2...origin/$1 | wc -l`
+    cd $INIT_DIR
+    return $COMMIT_NUMBER
+}
+
+tableCreateRowStart(){
+    echo "		<tr>" >> $OUTPUT_HANDLE
+}
+tableCreateRowEnd(){
+    echo "		</tr>" >> $OUTPUT_HANDLE
+}
+
+tableCreateCell(){
+    echo "			<td>$1</td>" >> $OUTPUT_HANDLE
+}
+
+tableCreateCellLink(){
+    echo "			<td><a href=\"$1\" target=\"_blank\">Link</a></td>" >> $OUTPUT_HANDLE
+}
+
+reportEnd(){
+    rm -rf $REPO_DIR
+    cat $REPORT_END >> "$OUTPUT_HANDLE"
+    END=$(date +%s)
+    DIFF=$(( $END - $START ))
+    echo "#################################################"
+    echo " Finalized report, access:"
+    echo " > $OUTPUT_HANDLE"
+    echo " > Duration: `date -u -d @$DIFF +\"%T\"`"
+    echo "#################################################"
+}
+
+###################
+#ACTION start here#
+###################
+if [ $# != 1 ]; then
+    echo 1>&2 "Usage: $0 <input_file.txt>"
+    exit 1
+fi
+echo "#################################################"
+echo " Initializing repository report:"
+echo " > $1"
+echo "#################################################"
+init_script
+
+
+while read component; do
+# component content
+# undertow-io-undertow-js;sdouglas@redhat.com;GIT;https://github.com/undertow-io/undertow.js.git;1.0.x;1.0.2.Final;io.undertow.js;$TAG_DIF_BRANCH;$LINES_OF_CODE;$COMMENT
+#     0                           1            2               3                                   4      5           6              7                 8              9  
+    IFS=';' read -ra PIECES <<< "$component"
+    tableCreateRowStart
+    tableCreateCell "${PIECES[0]}"
+    tableCreateCell "${PIECES[1]}"
+    tableCreateCell "${PIECES[2]}"
+    tableCreateCellLink "${PIECES[3]}"
+    tableCreateCell "${PIECES[4]}"
+    tableCreateCell "${PIECES[5]}"
+    tableCreateCell "${PIECES[6]}"
+
+    if [ "${PIECES[2]}" = "SVN" ]; then
+        checkoutSVN "${PIECES[3]}" "${PIECES[4]}"
+        #There is no way to calculate tag diff for svn?
+        tableCreateCell ""
+        loc=$(getLinesOfCode "${PIECES[0]}")
+        [[ $loc =~ _code_\/.+\.txt ]]
+        tableCreateCellLink "$BASH_REMATCH"
+    elif [ "${PIECES[2]}" = "GIT" ]; then
+        checkoutGIT "${PIECES[3]}"
+        getGITCommitsFromBranch "${PIECES[4]}" "${PIECES[5]}"
+        tableCreateCell "$?"
+        loc=$(getLinesOfCode "${PIECES[0]}")
+        #grep + regexp is required, since CLOC or shell adds some trash to 'loc'
+        [[ $loc =~ _code_\/.+\.txt ]]
+        # BASH_REMATCH contain result of above call
+        tableCreateCellLink "$BASH_REMATCH"
+    
+    else 
+        tableCreateCell "${PIECES[7]}"
+        tableCreateCellLink "${PIECES[8]}"
+    fi
+    #if [ ${#distro[@]} = 10 ]; then
+        tableCreateCell "${PIECES[9]}"
+    #fi
+    
+    tableCreateRowEnd
+done < $1
+
+reportEnd
